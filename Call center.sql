08f71610cb09f0d61f41d10733218e5219e618b6
piyusharya8700 <piyusharya3003@proton.me>
2026-08-22 21:35:47 +0530

    Create Call center.sql

---
+++ Call center.sql
@@ -0,0 +1,9 @@
+create table call_center_logs(
+call_id varchar(100),
+call_date date,
+call_time time,
+agent_name varchar(100),
+department varchar(100),
+answered_yn varhcar(5),	Resolved	Speed of Answer	AvgTalkDuration	Satisfaction rating
+
+)
