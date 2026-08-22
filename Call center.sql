ab4deaf2db91370ee96247c3a00b6a5aa9af451c
piyusharya8700 <piyusharya3003@proton.me>
2026-08-22 21:38:21 +0530

    Update Call center.sql

--- Call center.sql
+++ Call center.sql
@@ -1,3 +1,6 @@
+-- Creating table so that we can import our csv.
+-- i was wondering a lot why am i unable to import data but after doing a bit research it turned out that postgre sql requires the date format as YYYY-MM-DD
+
 create table call_center_logs(
 call_id varchar(100),
 call_date date,
